59eea9111d5f6fa66e76cc1e9eadb0a30db346b5
Aleksandr Pyatkin <av.pyatkin@yandex.ru>
2022-06-03 21:34:51 +0400

    Error Handeling

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -10,6 +7,15 @@ print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+is_on = True
+while is_on:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+        is_on = False
+    except KeyError:
+        print("Sorry, please insert only letters!")
+
+
